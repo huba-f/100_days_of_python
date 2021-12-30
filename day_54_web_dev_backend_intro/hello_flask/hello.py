9efcffa9577ddb94998e64858ea4e84e94f6b7f7
huba <huba0ferencz@gmail.com>
2021-12-30 17:17:43 +0200

    final

--- day_54_web_dev_backend_intro/hello_flask/hello.py
+++ day_54_web_dev_backend_intro/hello_flask/hello.py
@@ -5,8 +5,29 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<h1 style='text-align: center;'>Hello, World!</h1>" \
+           "<p>image below</p>" \
+           "<img src='https://media1.giphy.com/media/3o7aDfk8UmiioXaeoo/giphy.gif?cid" \
+           "=ecf05e47awmskon29oackpe30vf2b4emvvz0uyuqb05i0u5m&rid=giphy.gif&ct=g'> "
+
+
+def make_bold(function):
+    def wrapper():
+        return "<b>" + function() + "</b>"
+
+    return wrapper
+
+
+@app.route("/bye")
+@make_bold
+def bye():
+    return "bye"
+
+
+@app.route('/<name>/<int:number>')
+def greeting(name, number):
+    return f"O hi there {name + '2'}, you are {number}!"
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
