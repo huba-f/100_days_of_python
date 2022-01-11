5cae2c198f27878b0bfd369e3f010151320dd9fc
huba <huba0ferencz@gmail.com>
2022-01-11 06:40:57 +0200

    final

---
+++ day_60_post_request/html_forms_flask/main.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def homepage():
+	return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def receive_data():
+	username = request.form.get('username')
+	password = request.form.get('password')
+	if request.method == 'POST':
+		return f"<h1>Name: {username}, Password: {password}</h1>"
+	else:
+		render_template('index.html')
+if __name__ == "__main__":
+    app.run(debug=True)
